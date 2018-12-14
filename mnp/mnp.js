9f70d0241d80d8c1928b61c528040c56cae2b740
Anton <anton@adc.sh>
2018-12-14 23:36:04 +0300

    ret

--- mnp/mnp.js
+++ mnp/mnp.js
@@ -3,11 +3,11 @@ export default async function (context, req) {
     if (req.body) req.body = req.body.slice(0, 100)
     if (req.rawBody) req.rawBody = req.rawBody.slice(0, 100)
     const body = JSON.stringify(req, null, 2)
-    return { body }
+    return body
   } else if (req.method == 'POST') {
     if (req.body) req.body = `${req.body.slice(0, 30)}...`
     if (req.rawBody) req.rawBody = req.rawBody.slice(0, 200)
     const body = JSON.stringify(req, null, 2)
-    return { body }
+    return body
   }
 }
